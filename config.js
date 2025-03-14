a7aa4b0919186a5b0125d79ff71e88087e1eb621
子清 <120438383+ziqing888@users.noreply.github.com>
2025-03-14 18:37:52 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+export const config = {
+  ref_code: "",
+  num_ref: 500, // number of references
+  max_threads: 50, // number of threads
+
+  ////// Test key============No change
+  rpc: "https://1rpc.io/base",
+  //valid values: 2captcha, anticaptcha
+  TYPE_CAPTCHA: "2captcha",
+  API_KEY_2CAPTCHA: "xxx", //Go to: https://2captcha.com/?from=24402314
+  API_KEY_ANTI_CAPTCHA: "xxx", //Go to: https://getcaptchasolution.com/asmsypuay9
+  CAPTCHA_URL: "https://dashboard.layeredge.io/",
+  WEBSITE_KEY: "6LdS4sEqAAAAAPCYk08sXJmhem7o0hKVxz3s0jDS",
+};
