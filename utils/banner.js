8090944b69d954355187d13b5757d4a78370f1c1
子清 <120438383+ziqing888@users.noreply.github.com>
2025-03-14 19:17:31 +0800

    Create banner.js

---
+++ utils/banner.js
@@ -0,0 +1,7 @@
+const banner = `
+  \x1b[36m┌────────────────────────────┐\x1b[0m
+  \x1b[33m│ 关注x:                     │\x1b[0m
+  \x1b[34m│ https://x.com/qklxsqf      │\x1b[0m
+  \x1b[36m└────────────────────────────┘\x1b[0m
+`;
+export default banner;
